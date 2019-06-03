ce17e5e005f48c634745318ba812e281f2a92172
Nguyễn Văn Dĩnh <dinhnv@vnext.com.vn>
2019-06-03 16:04:44 +0700

    a

--- api/src/index.js
+++ api/src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!1111111'));
+app.get('/', (req, res) => res.send('Hello World!22222222'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 var mysql = require('mysql')
