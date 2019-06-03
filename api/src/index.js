18e3a1fd0dc7a11d62ce18128a50f75722d7e36a
Nguyễn Văn Dĩnh <dinhnv@vnext.com.vn>
2019-06-03 15:57:19 +0700

    aa

--- api/src/index.js
+++ api/src/index.js
@@ -5,3 +5,20 @@ const port = 3000
 app.get('/', (req, res) => res.send('Hello World!1111111'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+    host: 'mysql',
+    user: 'root',
+    password: 'root',
+    database: 'exam'
+})
+
+connection.connect()
+
+connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+    if (err) throw err
+
+    console.log('The solution is: ', rows[0].solution)
+})
+
+connection.end()
